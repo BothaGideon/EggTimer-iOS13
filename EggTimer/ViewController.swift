9f10ea6a8bb242c6de0c86850d5e97ae15568b1b
Gideon Botha <gideon.botha@entelect.co.za>
2021-04-19 12:36:50 +0200

    init app and finished project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    var timer = Timer()
+    var timeLeftInSeconds: Int = 60
+    var initialTime: Int = 30
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+  
+    @IBOutlet weak var eggProgress: UIProgressView!
+    @IBOutlet weak var eggReadiness: UILabel!
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        eggProgress.progress = 0
+        timeLeftInSeconds = eggTimes[hardness]!
+        initialTime = eggTimes[hardness]!
+        
+        eggReadiness.text = "Your \(hardness) egg is in progress..."
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+    }
+    
+    @objc func update() {
+        if(timeLeftInSeconds > 0) {
+            print("\(timeLeftInSeconds) seconds.")
+            timeLeftInSeconds -= 1
+            let progressPercentage = Float(timeLeftInSeconds) / Float(initialTime)
+            eggProgress.progress = 1 - progressPercentage
+            print(eggProgress.progress)
+        } else {
+            eggReadiness.text = "Done!"
+            eggProgress.progress = 1
+            timer.invalidate()
+        }
+    }
 
 }
